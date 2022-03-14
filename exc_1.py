87348cbc5c0cef9a3aabe1f17f679bc04ab1215c
hadeelghourani <prof.hadeelghourani@gmail.com>
2022-03-14 17:11:53 +0300

    Third modification

--- exc_1.py
+++ exc_1.py
@@ -1,25 +1,22 @@
-class PlaneException(Exception):
+class FuelShortage(Exception):
     def __init__(self, current_location, destination_location, fuel, min_fuel):
         self.current_location = current_location
         self.destination_location = destination_location
         self.current_fuel = fuel
         self.min_fuel_needed = min_fuel
-        
-    def __str__(self):
-        print(f"Current Location: {self.current_location[0]}:\
-            {self.current_location[1]}")
-        print(f"Destination Location: {self.destination_location[0]}:\
-            {self.destination_location[1]}")
-        print(f"Fuel we have: {self.current_fuel}")
-        print(f"Minimum fuel we need: {self.min_fuel_needed}")
-           
+        msg = "\nCurrent Location: "\
+            f"{self.current_location[0]}:{self.current_location[1]}\n"\
+            "Destination Location: "\
+            f"{self.destination_location[0]}:{self.destination_location[1]}" \
+            f"\nFuel we have: {self.current_fuel}\n"\
+            f"Minimum fuel we need: {self.min_fuel_needed}\n"
+        super().__init__(msg)
+
 
 class Plane(Exception):
-
     def __init__(self, x, y, fuel):
         self.current_location = [x, y]
         self.destination_location = []
-        self.current_location[x] = y
         self.fuel = fuel
         self.distance = 0
 
@@ -32,26 +29,26 @@ class Plane(Exception):
     def add_fuel(self, value=0):
         self.fuel += value
 
-    def total_kilometers(self, x2, y2):
-        return abs(abs(x2) - abs(self.current_location[0])) + \
-            abs(abs(self.current_location[1]) - + abs(y2))
+    def get_distance(self, x2, y2):
+        return abs(abs(x2) + abs(self.current_location[0])) + \
+            abs(abs(self.current_location[1]) + abs(y2))
 
     def fly(self, x2, y2):
         self.destination_location = [x2, y2]
-        kilometers = self.total_kilometers(x2, y2)
-        total_fuel_needed = kilometers * 4
-        minimum_fuel_needed = total_fuel_needed - self.fuel
+        distance = self.get_distance(x2, y2)
+        fuel_needed = distance * 4
+        minimum_fuel_needed = fuel_needed - self.fuel
 
-        if total_fuel_needed > self.fuel:
-            raise(PlaneException(self.current_location,
-                                 self.destination_location, self.fuel, 
-                                 minimum_fuel_needed))
-           
-        print("The amount of fuel: ", self.fuel)
-        print("The amount of used fuel: ", kilometers * 4)
-        print("The coverd distance: ", kilometers * 4)
-        print("The remaining fuel: ", self.get_plane_fuel() - kilometers * 4)
-    
+        if fuel_needed > self.fuel:
+            raise(FuelShortage(self.current_location,
+                               self.destination_location, 
+                               self.fuel, minimum_fuel_needed))
+        else:
+            print("The amount of fuel: ", self.fuel)
+            print("The amount of used fuel: ", distance * 4)
+            print("The coverd distance: ", distance * 4)
+            print("The remaining fuel: ", self.fuel - distance * 4)
+
     def __str__(self):
         print("Current Location: ", self.get_plam_location())
         print("Fuel: ", self.fuel())
@@ -60,7 +57,13 @@ class Plane(Exception):
 
 if __name__ == '__main__':
     plane_1 = Plane(0, 0, 22)
-    plane_1.fly(3, 4)
+    try:
+        plane_1.fly(3, 4)
+    except Exception as e:
+        print(e)
     plane_1.add_fuel(10)
-    plane_1.fly(10, 10)
+    try:
+        plane_1.fly(10, 10)
+    except Exception as e:
+        print(e)
     plane_1.fly(3, 5)
